5904ed48e5ea97e06d99f8de5a1aeb985f20982c
Scott Anderson <sra@diem.net>
2016-08-20 15:04:16 -0400

    Rancher cli 0.1.0

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -1,8 +1,8 @@
 class RancherCli < Formula
   desc "The Rancher CLI is a unified tool to manage your Rancher server"
   homepage "https://github.com/rancher/cli"
-  url "https://github.com/rancher/cli/archive/v0.0.2.tar.gz"
-  sha256 "1a1e790031ef199d75f4d8af11a9df0bf8178f7840de057702d42de47fdc662f"
+  url "https://github.com/rancher/cli/archive/v0.1.0.tar.gz"
+  sha256 "3fad04522eb4b689de191da6322907c79a6ef41b057e6cf34f1570ad0f58de00"
 
   depends_on "go" => :build
 
