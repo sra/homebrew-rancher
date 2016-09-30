a7a0009255b3f8e87b5ba9a077c7878cd8b87630
Scott Anderson <sra@diem.net>
2016-09-30 09:27:17 -0400

    v0.9.0

---
+++ Formula/rancher-compose.rb
@@ -0,0 +1,22 @@
+class RancherCompose < Formula
+  desc "Docker Compose compatible client to deploy to Rancher"
+  homepage "https://github.com/rancher/rancher-compose"
+  url "https://github.com/rancher/rancher-compose/archive/v0.9.0.tar.gz"
+  sha256 "e49a711eaee301b8cb1028dee6be50a4041ded1db2f01fc9506bbde80bd1006f"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/rancher/rancher-compose").install Dir["*"]
+    system "go", "build", "-ldflags",
+           "-w -X github.com/rancher/rancher-compose/version.VERSION=#{version}",
+           "-o", "#{bin}/rancher-compose",
+           "-v", "github.com/rancher/rancher-compose"
+    prefix.install_metafiles "src/github.com/rancher/rancher-compose"
+  end
+
+  test do
+    system bin/"rancher-compose", "help"
+  end
+end
